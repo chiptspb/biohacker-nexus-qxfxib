ee4f3199a6b3204cd37f3bbe336752691c28949e
chiptspb <chiptspb@pm.me>
2025-10-30 09:24:01 -0400

    Delete BioHacker-Nexus-2-main directory

--- BioHacker-Nexus-2-main/app/(tabs)/(home)/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect } from "react";
-import { Redirect } from "expo-router";
-
-export default function HomeScreen() {
-  return <Redirect href="/(tabs)/(home)/dashboard" />;
-}
