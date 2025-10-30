e0a2fd1da0fc2ab1560557ce1041177ac451df0b
chiptspb <chiptspb@pm.me>
2025-10-30 09:25:16 -0400

    Delete constants directory

--- constants/limits.ts
+++
@@ -1,3 +0,0 @@
-// constants/limits.ts
-export const FREE_MAX_PRODUCTS = 1;
-
--- constants/pricing.ts
+++
@@ -1,6 +0,0 @@
-export const MONTHLY_PRODUCT_ID = 'com.chiptspb.biohackernexus.premium.monthly';
-export const ANNUAL_PRODUCT_ID  = 'com.chiptspb.biohackernexus.premium.annual';
-export const MONTHLY_PRICE_TEXT = '$2.99/month';
-export const ANNUAL_PRICE_TEXT  = '$24.99/year';
-
-
